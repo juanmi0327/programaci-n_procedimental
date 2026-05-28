9af437739570838af68ed5d699062cdd52d7aa56
juanmi0327 <juanmanuelzapatavalencia2703@gmail.com>
2026-05-28 17:14:45 -0500

    feat: add contabilidad, inventario, pedidos, and usuario scripts and update html templates

---
+++ programaci-n_procedimental/restaurante v2/js/inventario.js
@@ -0,0 +1,4 @@
+let codigo_produc = document.getElementById("codigo_producto").value;
+let nombre_produc = document.getElementById("nombre_producto").value;
+let cantidad = document.getElementById("cantidad").value;
+let marca_produc = document.getElementById("marca_producto").value;
