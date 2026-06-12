3db26980a7acd3cdfb28733804ba7ebff41dc1c0
Juan Manuel Zapata <juanmanuelzapatavalencia2703@gmail.com>
2026-06-11 20:09:38 -0500

    Update pedidos.js

--- programaci-n_procedimental/restaurante v2/js/pedidos.js
+++ programaci-n_procedimental/restaurante v2/js/pedidos.js
@@ -7,3 +7,37 @@ let metodo_pago = document.getElementById("metodo_pago").value;
 let num_tarjeta = document.getElementById("num_tarjeta").value;
 let vencimiento = document.getElementById("vencimiento").value;
 let cvv = document.getElementById("cvv").value;
+
+function ValidarVentas(){
+    if(producto == '' || usuario == '' || direccion == '' || telefono == '' || cantidad == '' ||  num_tarjeta == '' || cvv == '')
+        console.log("Los campos están vacíos")
+    else{
+    if(producto != /[a-zA-Z]/){
+        console.log('Los datos son incorrectos')
+    }
+
+    if(usuario != /[a-zA-Z]/){
+        console.log('Los datos son incorrectos')
+    }
+
+    if(direccion != /[a-zA-Z-1-9]/){
+        console.log('Los datos son incorrectos')
+    }
+
+    if(telefono.length() >= 10 && telefono != /[1-9]/){
+        console.log('Los datos son incorrectos')
+    }
+
+    if(cantidad != /[1-9]/){
+        console.log('Los datos son incorrectos')
+    }
+
+    if(num_tarjeta != /[1-9]/){
+        console.log('Los datos son incorrectos')
+    }
+
+    if(cvv.length() >= 3 && cvv != /[1-9]/){
+        console.log('Los datos son incorrectos')
+    }
+}
+}
